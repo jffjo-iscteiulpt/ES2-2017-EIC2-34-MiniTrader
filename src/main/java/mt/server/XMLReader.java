96fe410d9f3498f558504fbc34ae1748bb77f2ed
Claudio Lopes <cjcls@iscte.pt>
2017-04-20 13:39:25 +0100

    Created XMLReader.java!

---
+++ src/main/java/mt/server/XMLReader.java
@@ -0,0 +1,14 @@
+package mt.server;
+
+public class XMLReader {
+
+	public XMLReader() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
